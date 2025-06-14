c8a5c3628f59c158de8243e0efc8a29cdb1f678b
ranjanZ <ranjan.rbc@gmail.com>
2025-06-15 00:35:16 +0530

    Testing EC2

--- infra/dev/main.tf
+++ infra/dev/main.tf
@@ -23,22 +23,33 @@ resource "aws_instance" "nano_instance" {
   count         = length(data.aws_instances.existing.ids) == 0 ? 1 : 0
   ami           = "ami-0d1b5a8c13042c939"
   instance_type = "t2.nano"
-  user_data = filebase64("infra/start_script.sh")
+  
+  # Corrected user_data path and using file() instead of filebase64()
+  user_data = file("${path.module}/start_script.sh")
 
   tags = {
     Name = "Nano-EC2-Instance"
   }
 }
 
-# Stop the instance if it exists (whether newly created or existing)
-resource "aws_ec2_instance_state" "nano_stop" {
-  instance_id = length(data.aws_instances.existing.ids) > 0 ? data.aws_instances.existing.ids[0] : aws_instance.nano_instance[0].id
-  state       = "stopped"
+# Correct way to stop an instance (using aws_instance resource)
+resource "null_resource" "stop_instance" {
+  triggers = {
+    instance_id = length(data.aws_instances.existing.ids) > 0 ? data.aws_instances.existing.ids[0] : aws_instance.nano_instance[0].id
+  }
+
+  provisioner "local-exec" {
+    command = "aws ec2 stop-instances --instance-ids ${self.triggers.instance_id} --region us-east-2"
+  }
 }
 
 output "public_ip" {
   value = length(data.aws_instances.existing.ids) > 0 ? data.aws_instances.existing.public_ips[0] : aws_instance.nano_instance[0].public_ip
 }
+
+
+
+
 
 
 
