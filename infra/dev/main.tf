946f899b0917b626a5f5f93697ba2814709e0b0c
ranjanZ <ranjan.rbc@gmail.com>
2025-06-14 20:44:32 +0530

    one step ahead to infra

---
+++ infra/dev/main.tf
@@ -0,0 +1,28 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+
+resource "aws_budgets_budget" "monthly-budget" {
+  name              = "monthly-budget"
+  budget_type       = "COST"
+  limit_amount      = "0.001"
+  limit_unit        = "USD"
+  time_period_start = "2024-09-01_00:00"
+  time_unit         = "MONTHLY"
+
+}
+
+
+
+
