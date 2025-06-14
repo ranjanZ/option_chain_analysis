421ac49dbf79253c889406a8a85f5172f1004a88
ranjanZ <ranjan.rbc@gmail.com>
2025-06-15 02:38:59 +0530

    Testing EC2

--- infra/dev/main.tf
+++ infra/dev/main.tf
@@ -17,22 +17,31 @@ provider "aws" {
 data "aws_instances" "existing_nano" {
   filter {
     name   = "tag:Name"
-    values = ["Nano-EC2-Instance"]  
+    values = ["Nano-EC2-Instance"]
+  }
+  filter {
+    name   = "instance-state-name"
+    values = ["running", "stopped"]  # Only count active instances
   }
 }
 
 resource "aws_instance" "nano_instance" {
+  # Only create if NO matching instance exists
+  count = length(data.aws_instances.existing_nano.ids) == 0 ? 1 : 0
+
   ami           = "ami-0d1b5a8c13042c939"
   instance_type = "t2.nano"
+  
   tags = {
     Name = "Nano-EC2-Instance"
   }
 
   lifecycle {
+    prevent_destroy = true  # Extra protection against deletion
     ignore_changes = [ami, instance_type]  # Prevents recreation if specs change
-    prevent_destroy = true                  # Blocks accidental deletion
   }
 }
+
 
 
 #*/
