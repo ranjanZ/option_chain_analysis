281ab32c6e28b64240cf7d99b7ea9b2eafd72ae7
ranjanZ <ranjan.rbc@gmail.com>
2025-06-14 21:00:49 +0530

    0.001$  I use free only

--- infra/dev/main.tf
+++ infra/dev/main.tf
@@ -13,13 +13,30 @@ provider "aws" {
 }
 
 
+resource "null_resource" "budget_check" {
+  triggers = {
+    budget_exists = <<EOT
+      #!/bin/bash
+      aws budgets describe-budgets --account-id 992382828607 \
+        --query 'Budgets[?Name==`monthly-budget`].Name' \
+        --output text | grep -q monthly-budget && echo "exists"
+    EOT
+  }
+}
+
 resource "aws_budgets_budget" "monthly-budget" {
-  name              = "monthly-budget"
-  budget_type       = "COST"
-  limit_amount      = "0.001"
-  limit_unit        = "USD"
-  time_period_start = "2024-09-01_00:00"
-  time_unit         = "MONTHLY"
+  count         = null_resource.budget_check.triggers.budget_exists == "exists" ? 0 : 1
+  name          = "monthly-budget"
+  budget_type   = "COST"
+  limit_amount  = "0.001"
+  limit_unit    = "USD"
+  time_unit     = "MONTHLY"
+}
+
+
+
+
+
 
 }
 
