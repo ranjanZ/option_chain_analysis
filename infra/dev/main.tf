44370df44cdf7aa57fa7266ec9d03099dfe20a47
ranjanZ <ranjan.rbc@gmail.com>
2025-06-15 19:25:26 +0530

    infa chanced

--- infra/dev/main.tf
+++ infra/dev/main.tf
@@ -15,6 +15,31 @@ provider "aws" {
 
 
 resource "aws_instance" "nano_instance" {
+  count         = 1
+  ami           = "ami-0d1b5a8c13042c939"  # Ubuntu 20.04 LTS
+  instance_type = "t2.nano"
+  
+  # Reference external script file
+  user_data = file("${path.module}/start_script.sh")
+
+  tags = {
+    Name = "Nano-EC2-Instance"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+    ignore_changes = [ami, instance_type, user_data]  # Preserve user_data changes
+  }
+}
+
+
+
+
+
+
+/*
+
+resource "aws_instance" "nano_instance" {
   # Only create if NO matching instance exists
   count 	= 1  
   ami           = "ami-0d1b5a8c13042c939"
@@ -33,7 +58,7 @@ resource "aws_instance" "nano_instance" {
 
 
 
-
+*/
 
 
 
--- infra/dev/start_script.sh
+++ infra/dev/start_script.sh
@@ -1,2 +1,12 @@
-apt-get update
-apt-get install docker
+sudo apt-get update
+sudo apt-get install docker
+
+
+sudo snap install amazon-ssm-agent --classic
+sudo systemctl enable amazon-ssm-agent
+sudo systemctl start amazon-ssm-agent
+
+
+sudo systemctl restart amazon-ssm-agent
+
+
